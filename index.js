ba2aec9e956f618dc2374c415af8366d1f607b92
Jordan <JBuren1957@Gmail.com>
2022-06-07 20:36:53 -0500

    commit tree bc59606922291a41bc7afc459dc90e2ed17b17bf

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(event = "roller-skate") {
+return `This Saturday, I want to ${event}!`
+}
+function mondayWork(event = "go to the office") {
+return `This Monday, I will ${event}.`
+}
+function wrapAdjective(x="*") {
+    const msg = "You are"
+    return function
+    innerFunction(x2="special") {
+        return `${msg} ${x}${x2}${x}!`
+    }
+}
